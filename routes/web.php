8712445b62d4784b9f3f231c26df4e7f77bd83e8
HendriCahyani <hendrirainsclouds30@gmail.com>
2022-08-12 09:51:30 +0800

    add auth

--- routes/web.php
+++ routes/web.php
@@ -20,27 +20,28 @@ use App\Http\Controllers\InstructorController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-route::get('/Student',[StudentController::class,'index']);
-route::get('/Student/create',[StudentController::class,'create']);
-route::post('/Student/store',[StudentController::class,'store']);
-route::get('/Student/{id}/edit',[StudentController::class,'edit']);
-route::put('/Student/{id}',[StudentController::class,'update']);
-route::delete('/Student/{id}',[StudentController::class,'destroy']);
-
-route::get('/Car',[CarController::class,'index']);
-route::get('/Car/create',[CarController::class,'create']);
-route::post('/Car/store',[CarController::class,'store']);
-route::get('/Car/{id}/edit',[CarController::class,'edit']);
-route::put('/Car/{id}',[CarController::class,'update']);
-route::delete('/Car/{id}',[CarController::class,'destroy']);
-
-route::get('/Instructor',[InstructorController::class,'index']);
-route::get('/Instructor/create',[InstructorController::class,'create']);
-route::post('/Instructor/store',[InstructorController::class,'store']);
-route::get('/Instructor/{id}/edit',[InstructorController::class,'edit']);
-route::put('/Instructor/{id}',[InstructorController::class,'update']);
-route::delete('/Instructor/{id}',[InstructorController::class,'destroy']);
+route::middleware(['auth'])->group(function(){
+    route::get('/Student',[StudentController::class,'index']);
+    route::get('/Student/create',[StudentController::class,'create']);
+    route::post('/Student/store',[StudentController::class,'store']);
+    route::get('/Student/{id}/edit',[StudentController::class,'edit']);
+    route::put('/Student/{id}',[StudentController::class,'update']);
+    route::delete('/Student/{id}',[StudentController::class,'destroy']);
+    
+    route::get('/Car',[CarController::class,'index']);
+    route::get('/Car/create',[CarController::class,'create']);
+    route::post('/Car/store',[CarController::class,'store']);
+    route::get('/Car/{id}/edit',[CarController::class,'edit']);
+    route::put('/Car/{id}',[CarController::class,'update']);
+    route::delete('/Car/{id}',[CarController::class,'destroy']);
+    
+    route::get('/Instructor',[InstructorController::class,'index']);
+    route::get('/Instructor/create',[InstructorController::class,'create']);
+    route::post('/Instructor/store',[InstructorController::class,'store']);
+    route::get('/Instructor/{id}/edit',[InstructorController::class,'edit']);
+    route::put('/Instructor/{id}',[InstructorController::class,'update']);
+    route::delete('/Instructor/{id}',[InstructorController::class,'destroy']);
+});
 
 Auth::routes();
 
